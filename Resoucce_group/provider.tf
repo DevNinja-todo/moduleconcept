f04bdc037f9572414365bfdb3bf66d9c1ce6232a
Jay Prakash yadav <jpyadav256@outlook.com>
2024-09-15 14:07:59 +0530

    rg remove

--- Resoucce_group/provider.tf
+++ Resoucce_group/provider.tf
@@ -9,15 +9,5 @@ terraform {
 
 provider "azurerm" {
   features {
-    
   }
-}
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
-}
-
-resource "azurerm_resource_group" "example1" {
-  name     = "example1"
-  location = "West Europe"
-}
+  }
