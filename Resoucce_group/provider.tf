e7cb0fd965ae21fee701c140b50f3a2480582a75
Jay Prakash yadav <jpyadav256@outlook.com>
2024-09-15 14:09:53 +0530

    two rg added

--- Resoucce_group/provider.tf
+++ Resoucce_group/provider.tf
@@ -11,3 +11,13 @@ provider "azurerm" {
   features {
   }
   }
+
+  resource "azurerm_resource_group" "example1" {
+  name     = "example1"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example2"
+  location = "West Europe"
+}
